76025738933691f6bf51891b4219a02e6e59eb94
DylanBrss <dylanbrisseau79@gmail.com>
2023-01-13 15:45:26 +0100

    dev de la partie projects

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 import Qualifications from "./components/qualifications/Qualifications";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
@@ -22,6 +23,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Portfolio />
         <Qualifications />
         <Testimonials />
         <Contact />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
                         </li>
 
                         <li className="nav__item">
-                            <a href="#portfolio" onClick={() => setActiveNav("#portfolio")} className={activeNav === "#portfolio" ? "nav__link active-link" : "nav__link"}>
+                            <a href="#projects" onClick={() => setActiveNav("#projects")} className={activeNav === "#projects" ? "nav__link active-link" : "nav__link"}>
                                 <i className="uil uil-scenery nav__icon"></i> Portfolio
                             </a>
                         </li>
